733102d8c238e97735abb57003e131c2c3b5bc00
mankmanky1 <mankmanky1>
2010-04-28 18:19:19 +0200

    SQL

--- Dribble_Server/Dribble_Processing-ejb/src/java/dribble/dataset/SQLCommunicator.java
+++ Dribble_Server/Dribble_Processing-ejb/src/java/dribble/dataset/SQLCommunicator.java
@@ -3,13 +3,13 @@ package dribble.dataset;
 import dribble.common.*;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
  *
@@ -21,30 +21,37 @@ public class SQLCommunicator implements Dataset {
     private String connectionUrl;
     private Connection con;
 
+    //Initialise the database connection in constructor
     public SQLCommunicator() {
         logger.info("Inside SQLConstructor");
         try {
-
+            //Connect to the database
             connectionUrl = "jdbc:derby://localhost:1527/DribbleDerbyDB";
+            
             con = DriverManager.getConnection(connectionUrl, "APP", "dribble");
-
+            logger.info("Connected to Glassfish database");
+            DatabaseMetaData md = con.getMetaData();
+            //Check if the table exists in the database
+            ResultSet rs = md.getTables(null, "APP", "%", null);
+            //Add a table if the table does not exist
+            if(rs.next()==false)
+            {
             Statement stmt = con.createStatement();
-            logger.info("Connection Started");
-            ResultSet rs = stmt.executeQuery("SELECT ID "
-                    + "FROM DRIBBLE_SYSTEM_SUBJECTS ");
-
-            if (rs.next() == false) {
-                logger.info("Creating new subject table");
-                stmt.execute("CREATE TABLE \"DRIBBLE_SYSTEM_SUBJECTS\""
-                        + "(DRIB VARCHAR(144),"
-                        + "LAT BIGINT,"
-                        + "LONG BIGINT,"
-                        + "CURRENTIME BIGINT,"
-                        + "DRIBID BIGINT,"
-                        + "DRIBPOPULARITY BIGINT"
+            logger.info("No table in database. Creating main table...");
+            stmt.execute("CREATE TABLE DRIBBLE_SYSTEM_SUBJECTS"+
+                         "(NAME VARCHAR(20),"
+                        + "ID INTEGER NOT NULL,"
+                        + "LATITUDE BIGINT,"
+                        + "LONGITUDE BIGINT,"
+                        + "VIEWS INTEGER default 0,"
+                        + "POSTS INTEGER default 0,"
+                        + "POPULARITY BIGINT default 0,"
+                        + "CURRENTIME BIGINT"
                         + ")");
-
-                logger.info("Subject table created");
+           }
+            else
+            {
+                logger.info("Table exists in database");
             }
 
 
@@ -55,28 +62,29 @@ public class SQLCommunicator implements Dataset {
         logger.info("Constructed SQLCommunicator");
 
     }
-
+    //Adds a drib to the database
     public boolean addDrib(Drib m) {
 
-        logger.info("Add Drib");
+        logger.info("Adding a Drib to the database...");
 
         try {
 
             Statement stmt = con.createStatement();
-            logger.info("Connection Started");
+            logger.info("Add Drib connection established");
             ResultSet rs = stmt.executeQuery("SELECT ID "
                     + "FROM DRIBBLE_SYSTEM_SUBJECTS "
                     + "WHERE ID=" + m.getSubject().getSubjectID());
 
             if (rs.next() == false) {
-                logger.info("Creating new subject table");
+                logger.info("DribSubject does not exist. Creating new subject table...");
                 stmt.execute("CREATE TABLE \"" + m.getSubject().getSubjectID() + "\""
                         + "(DRIB VARCHAR(144),"
                         + "LAT BIGINT,"
                         + "LONG BIGINT,"
                         + "CURRENTIME BIGINT,"
                         + "DRIBID BIGINT,"
-                        + "DRIBPOPULARITY BIGINT"
+                        + "DRIBLIKE BIGINT default 0,"
+                        + "DRIBPOPULARITY BIGINT default 0"
                         + ")");
 
                 logger.info("Subject table created");
@@ -84,15 +92,25 @@ public class SQLCommunicator implements Dataset {
                 stmt.execute("INSERT INTO DRIBBLE_SYSTEM_SUBJECTS" + " (NAME,ID,LATITUDE,LONGITUDE,VIEWS,POSTS,POPULARITY,CURRENTIME)"
                         + "VALUES (" + "\'" + m.getSubject().getName() + "\'," + m.getSubject().getSubjectID()
                         + "," + m.getSubject().getLatitude() + "," + m.getSubject().getLongitude()
-                        + "," + m.getSubject().getNumViews() + "," + m.getSubject().getNumPosts()
-                        + "," + m.getSubject().getPopularity() + "," + m.getSubject().getTime() + ")");
+                        + "," + 0 + "," + 0
+                        + "," + 0 + "," + m.getSubject().getTime() + ")");
             }
 
             logger.info("Adding Drib to subject table");
-            stmt.execute("INSERT INTO \"" + m.getSubject().getSubjectID() + "\" (DRIB,LAT,LONG,CURRENTIME,DRIBID,DRIBPOPULARITY)"
-                    + "VALUES (" + "\'" + m.getText() + "\'" + "," + m.getLatitude() + "," + m.getLongitude() + "," + m.getTime() + "," + m.getMessageID() + "," + m.getPopularity() + ")");
+            stmt.execute("INSERT INTO \"" + m.getSubject().getSubjectID() + "\" (DRIB,LAT,LONG,CURRENTIME,DRIBID,DRIBLIKE, DRIBPOPULARITY)"
+                    + "VALUES (" + "\'" + m.getText() + "\'" + "," + m.getLatitude() + "," + m.getLongitude() + "," + m.getTime() + "," + m.getMessageID()+ "," + m.getLikeCount() + "," + m.getPopularity() + ")");
 
+            stmt.execute("UPDATE DRIBBLE_SYSTEM_SUBJECTS SET CURRENTIME = "+m.getTime()+" WHERE ID = "+m.getSubject().getSubjectID());
             logger.info("Drib successfully added to table");
+
+            //Incrementing the number of posts of a subject
+            ResultSet updatepost = stmt.executeQuery("SELECT POSTS "
+                    + "FROM DRIBBLE_SYSTEM_SUBJECTS "
+                    + "WHERE ID=" + m.getSubject().getSubjectID());
+            updatepost.next();
+            int num_posts = updatepost.getInt("POSTS");
+
+            stmt.execute("UPDATE DRIBBLE_SYSTEM_SUBJECTS SET POSTS = "+(num_posts+1)+"WHERE ID = "+ m.getSubject().getSubjectID());
             
             return true;
 
@@ -103,45 +121,61 @@ public class SQLCommunicator implements Dataset {
 
 
     }
+    //Get a list of Drib messages for a particular subject
+    public ArrayList<Drib> getDribs(int subjectID, long lat, long longitude, long radius) {
 
-    public ArrayList<Drib> getDribs(DribSubject s, double lat, double longitude, double radius) {
-
-        logger.info("Get Dribs");
+        logger.info("Getting Drib messages for a particular subject...");
 
         ArrayList<Drib> DribList = new ArrayList<Drib>();
-        double latlimit1 = lat + radius;
-        double latlimit2 = lat - radius;
-        double longlimit1 = longitude + radius;
-        double longlimit2 = longitude - radius;
+        long latlimit1 = lat + radius;
+        long latlimit2 = lat - radius;
+        long longlimit1 = longitude + radius;
+        long longlimit2 = longitude - radius;
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT FROM " + s.getSubjectID()
-                    + "WHERE LAT <" + latlimit1
+            logger.info("Get Dribs connection established");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM \"" + subjectID
+                    + "\" WHERE LAT <" + latlimit1
                     + "AND LAT >" + latlimit2
                     + "AND LONG <" + longlimit1
                     + "AND LONG >" + longlimit2);
+            //Add Dribs found in table to the dribs list to be sent to the user
             while (rs.next()) {
-                Drib m = getDrib(s, rs.getInt("DRIBID"));
+                Drib m = getDrib(subjectID, rs.getInt("DRIBID"));
                 DribList.add(m);
             }
+            //incrementing number of views for a subject
+            ResultSet updatepost = stmt.executeQuery("SELECT VIEWS "
+                    + "FROM DRIBBLE_SYSTEM_SUBJECTS "
+                    + "WHERE ID=" + subjectID);
+            updatepost.next();
+            int num_views = updatepost.getInt("VIEWS");
+
+            stmt.execute("UPDATE DRIBBLE_SYSTEM_SUBJECTS SET VIEWS = "+(num_views+1)+"WHERE ID = "+ subjectID);
+
         } catch (SQLException e) {
             logger.severe("Error getting Dribs: " + e.getMessage());
         }
         return DribList;
     }
 
-    public ArrayList<DribSubject> getDribSubjects(double lat, double longitude, double radius) {
+    //Get all the DribSubjects available in a specific region
+    public ArrayList<DribSubject> getDribSubjects(long lat, long longitude, long radius) {
 
-        logger.info("Get Drib Subjects");
+        logger.info("Getting all the DribSubjects...");
 
         ArrayList<DribSubject> DribList = new ArrayList<DribSubject>();
         try {
 
             Statement stmt = con.createStatement();
-            ResultSet topics = stmt.executeQuery("SELECT ID FROM DRIBBLE_SYSTEM_DRIBSUBJECTS");
-            while (topics.next()) {
-                if (!(getDribs(getDribSubject(topics.getInt("ID")), lat, longitude, radius).isEmpty())) {
-                    DribList.add(getDribSubject(topics.getInt("ID")));
+            logger.info("Connection Established");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM DRIBBLE_SYSTEM_SUBJECTS");
+            
+            //Add the drib subjects to a drib subject list
+            while (rs.next()) {
+                if (!(getDribs(rs.getInt("ID"), lat, longitude, radius).isEmpty())) {
+                    logger.info("Subject Found");
+                    DribList.add(getDribSubject(rs.getInt("ID")));
                 }
             }
 
@@ -153,9 +187,9 @@ public class SQLCommunicator implements Dataset {
 
     }
 
+    //Update the Drib
     public boolean updateDrib(Drib m) {
-        //Deletes Original Drib and updates with the new one
-        //This is not the most efficient way but is very simple and elegent
+   
         deleteDrib(m);
         addDrib(m);
 
@@ -163,20 +197,32 @@ public class SQLCommunicator implements Dataset {
 
         return true;
     }
-
+    
+    //Remove a Drib
     public boolean deleteDrib(Drib m) {
 
-        logger.info("Deleting Drib");
+        logger.info("Deleting Drib...");
 
         try {
             Statement stmt = con.createStatement();
-            stmt.execute("DELETE FROM " + m.getSubject().getSubjectID()
-                    + "WHERE DRIBID=" + m.getMessageID());
+            stmt.execute("DELETE FROM \"" + m.getSubject().getSubjectID()
+                    + "\" WHERE DRIBID=" + m.getMessageID());
+
+
+            //Subtract the number of posts from a subject
+            ResultSet updatepost = stmt.executeQuery("SELECT POSTS "
+                    + "FROM DRIBBLE_SYSTEM_SUBJECTS "
+                    + "WHERE ID=" + m.getSubject().getSubjectID());
+            updatepost.next();
+            int num_posts = updatepost.getInt("POSTS");
+
+            stmt.execute("UPDATE DRIBBLE_SYSTEM_SUBJECTS SET POSTS = "+(num_posts-1)+"WHERE ID = "+ m.getSubject().getSubjectID());
+
 
             if (m.getSubject().getNumPosts() == 1) {
-                stmt.execute("DELETE FROM  DRIBBLE_SYSTEM_DRIBSUBJECTS"
+                stmt.execute("DELETE FROM  DRIBBLE_SYSTEM_SUBJECTS"
                         + "WHERE ID=" + m.getSubject().getSubjectID() + ")");
-                stmt.execute("(DROP TABLE" + m.getSubject().getSubjectID() + ")");
+                stmt.execute("(DROP TABLE \"" + m.getSubject().getSubjectID() + "\")");
             }
 
             logger.info("Drib deleted");
@@ -190,27 +236,40 @@ public class SQLCommunicator implements Dataset {
             return false;
         }
     }
-
-    public Drib getDrib(DribSubject s, int DribID) {
+    //Return a specific Drib
+    public Drib getDrib(int subjectID, int DribID) {
 
         logger.info("Getting Drib");
 
-        Drib m = new Drib(s, "default", 0, 0);
+        //Create a new Drib object to hold the information of the requested Drib
+        Drib message = new Drib();
+        message.setSubject(getDribSubject(subjectID));
 
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT FROM" + s.getSubjectID()
-                    + "WHERE DRIBID=" + DribID);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM \"" + subjectID
+                    + "\" WHERE DRIBID=" + DribID);
+            rs.next();
+
+            logger.info("Drib selected from database and updating...");
+
+            //Populating a drib with information
             String DribText = rs.getString("DRIB");
-            m.setText(DribText);
-            double lat = rs.getDouble("LAT");
-            m.setLatitude(lat);
-            double longitude = rs.getDouble("LONG");
-            m.setLongitude(longitude);
-            Date time = rs.getDate("TIME");
-            m.setTime(time.getTime());
+            logger.info("The string is "+ DribText);
+            message.setText(DribText);
+            long lat = rs.getLong("LAT");
+            message.setLatitude(lat);
+            long longitude = rs.getLong("LONG");
+            message.setLongitude(longitude);
+            long time = rs.getLong("CURRENTIME");
+            message.setTime(time);
+            int dribid = rs.getInt("DRIBID");
+            message.setMessageID(dribid);
+            int driblike = rs.getInt("DRIBLIKE");
+            message.setLikeCount(driblike);
             int popularity = rs.getInt("DRIBPOPULARITY");
-            m.setPopularity(popularity);
+            message.setPopularity(popularity);
+            logger.info("Drib populated... Returning Drib");
 
         } catch (SQLException e) {
 
@@ -219,35 +278,40 @@ public class SQLCommunicator implements Dataset {
             return null;
         }
 
-        logger.info("Got Drib");
-
-        return m;
+        return message;
     }
 
-    public DribSubject getDribSubject(int SubjectID) {
-        logger.info("Getting Subject");
+  //Return a requested DribSubject
+  public DribSubject getDribSubject(int SubjectID) {
+        logger.info("Getting the requested DribSubject");
 
-        DribSubject s = new DribSubject();
+        DribSubject subject = new DribSubject();
 
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM DRIBBLE_SYSTEM_DRIBSUBJECTS"
-                    + "WHERE ID=" + SubjectID);
+            //Find the requested subject in the database
+            ResultSet rs = stmt.executeQuery("SELECT * FROM DRIBBLE_SYSTEM_SUBJECTS "
+                    + "WHERE ID = " + SubjectID);
+            logger.info("Added name field to subject object");
+            rs.next();
             String name = rs.getString("NAME");
-            s.setName(name);
-            double lat = rs.getDouble("LAT");
-            s.setLatitude(lat);
-            double longitude = rs.getDouble("LONG");
-            s.setLongitude(longitude);
+            subject.setName(name);
+
+            //Populate the DribSubject Object
+            subject.setSubjectID(SubjectID);
+            long lat = rs.getLong("LATITUDE");
+            subject.setLatitude(lat);
+            long longitude = rs.getLong("LONGITUDE");
+            subject.setLongitude(longitude);
             int views = rs.getInt("VIEWS");
-            s.setNumViews(views);
+            subject.setNumViews(views);
             int posts = rs.getInt("POSTS");
-            s.setNumViews(posts);
-            Date time = rs.getDate("TIME");
-            s.setTime(time);
+            subject.setNumViews(posts);
             int popularity = rs.getInt("POPULARITY");
-            s.setPopularity(popularity);
-
+            subject.setNumViews(posts);
+            long time = rs.getLong("CURRENTIME");
+            subject.setTime(time);
+            logger.info("DribSubject populated... Returning DribSubject");
         } catch (SQLException e) {
 
             logger.severe("Error getting subject: " + e.getMessage());
@@ -255,31 +319,7 @@ public class SQLCommunicator implements Dataset {
             return null;
         }
 
-        logger.info("Got subject");
-
-        return s;
+        return subject;
     }
-
-    public DribSubject getDribSubject(String SubjectName) {
-        logger.info("Getting Subject");
-
-        DribSubject s = new DribSubject();
-
-        try {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT ID FROM DRIBBLE_SYSTEM_DRIBSUBJECTS"
-                    + "WHERE NAME=" + SubjectName);
-            s = getDribSubject(rs.getInt("ID"));
-
-        } catch (SQLException e) {
-
-            logger.severe("Error getting subject: " + e.getMessage());
-
-            return null;
-        }
-
-        logger.info("Got subject");
-
-        return s;
-    }
+    
 }
